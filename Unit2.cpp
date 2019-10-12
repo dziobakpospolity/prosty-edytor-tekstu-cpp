f58720a17a253b3f92fda9dd85df3d2d588f8aed
imjustastudent <54544519+dziobakpospolity@users.noreply.github.com>
2019-10-12 18:20:10 +0200

    Update Unit2.cpp

--- Unit2.cpp
+++ Unit2.cpp
@@ -10,7 +10,7 @@ TDocument::TDocument(TPageControl *APageControl): TTabSheet(APageControl)
 {
  PageControl = APageControl;
  Caption = "Nowy dokument";
- /* s³ówko this oznacza odwo³anie siê do konkretnej instancji obiektu
+ /* sÅ‚Ã³wko this oznacza odwoÅ‚anie siÄ™ do konkretnej instancji obiektu
 klasy TDocument*/
  RichEdit = new TRichEdit(this);
  RichEdit->Parent = this;
@@ -23,8 +23,8 @@ void TDocument::LoadFromFile(String AFileName)
 {
  FileName = AFileName;
  RichEdit->Lines->LoadFromFile(FileName);
- /*Procedura ExtractFileName pozwala na wyci¹gniecie samej nazwy pliku z
-pe³nej œcie¿ki dostêpu. Nazwa zak³adki bêdzie nazw¹ pliku.*/
+ /*Procedura ExtractFileName pozwala na wyciÄ…gniecie samej nazwy pliku z
+peÅ‚nej Å›cieÅ¼ki dostÄ™pu. Nazwa zakÅ‚adki bÄ™dzie nazwÄ… pliku.*/
  Caption = ExtractFileName(FileName);
 }
 
